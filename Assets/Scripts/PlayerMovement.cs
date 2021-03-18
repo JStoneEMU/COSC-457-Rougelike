c532e97b339c38f55c9a43d3f8e43e4423ff5d54
JStoneEMU <78188057+JStoneEMU@users.noreply.github.com>
2021-03-17 21:03:48 -0400

    Testing Package Created

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update() //Put all input in here
+    {
+        movement.x = Input.GetAxisRaw("Horizontal"); //Left input gives -1, right givs 1, nothing gives 0
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate() 
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);//Time.fixedDeltaTime ensures our movement speed is consistent no matter how many times FixedUpdate is called
+    }
+}
