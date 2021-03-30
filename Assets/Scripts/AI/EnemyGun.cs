f8d50ff85f19468508277b00cba6c3454c4ea886
Michael Neet <mneet@emich.edu>
2021-03-30 19:59:00 -0400

    moved ai scripts to separate folder

---
+++ Assets/Scripts/AI/EnemyGun.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGun : MonoBehaviour
+{
+    GameObject projectile;
+    public float firingCooldown = 1;
+
+    public void Fire(Vector2 direction)
+    {
+
+    }
+}
