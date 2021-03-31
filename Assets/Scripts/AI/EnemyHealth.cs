0c0f10d63486f0e065eeaaa6e1dd7090bbd6deb7
Michael Neet <mneet@emich.edu>
2021-03-31 01:15:24 -0400

    renamed EnemyDamage to EnemyHealth

--- Assets/Scripts/AI/EnemyHealth.cs
+++ Assets/Scripts/AI/EnemyHealth.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyDamage : MonoBehaviour
+public class EnemyHealth : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D collision)
     {
