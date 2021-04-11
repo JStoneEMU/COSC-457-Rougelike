74252e9ac31f2eeb2b1f04057b5a2fa13bb83d54
Michael Neet <mneet@emich.edu>
2021-04-11 19:27:42 -0400

    added enemy health bars

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,27 +6,35 @@ public class Enemy : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject bulletPrefab;
+    public GameObject healthBarObj;
 
     public float moveSpeed = 5f;
-    public float maxHealth = 10f;
+    public int maxHealth = 10;
     public float bulletForce = 2f;
+    public float healthBarDuration = 2f;
 
     // Currently only used for minimax search
     public int attackRange = 10;
     public int attackDamage = 10;
 
-    public float CurrentHealth { get; set; }
+    public int CurrentHealth { get; set; }
     // Object being looked at, if null rotation is based on movement
     public GameObject LookingAt { get; set; }
 
     private Rigidbody2D rb;
     private float initialRotation;
+    private HealthBar healthBarScript;
+    private float healthBarTimer = 0;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         CurrentHealth = maxHealth;
         transform.rotation.ToAngleAxis(out initialRotation, out var axis);
+        healthBarScript = healthBarObj.GetComponent<HealthBar>();
+        healthBarScript.SetMaxHealth(maxHealth);
+        if (healthBarDuration > 0)
+            HideHealthBar();
     }
 
     void Update()
@@ -42,6 +50,15 @@ public class Enemy : MonoBehaviour
             Vector2 lookDir = lookPos - rb.position;
             float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg + 0f;
             rb.rotation = initialRotation + angle;
+        }
+
+        if (healthBarTimer > 0)
+        {
+            healthBarTimer -= Time.deltaTime;
+        }
+        else
+        {
+            HideHealthBar();
         }
     }
 
@@ -88,7 +105,20 @@ public class Enemy : MonoBehaviour
             if (bullet != null && bullet.Source != gameObject)
             {
                 CurrentHealth -= 10;
+                ShowHealthBar();
             }
         }
+    }
+
+    private void ShowHealthBar()
+    {
+        healthBarObj.SetActive(true);
+        healthBarScript.SetHealth(CurrentHealth);
+        healthBarTimer = healthBarDuration;
+    }
+
+    private void HideHealthBar()
+    {
+        healthBarObj.SetActive(false);
     }
 }
